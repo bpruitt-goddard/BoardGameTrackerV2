16626d8c336caf1c16d1215867723819ecbb1f30
Brian Pruitt-Goddard <pruittgoddard.b@gmail.com>
2018-09-25 22:34:49 -0700

    Feat: Add player table create db migration

---
+++ BoardGameTrackerV2/Data/Migrations/20180926053424_CreatePlayer.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BoardGameTrackerV2.Data.Migrations
+{
+    public partial class CreatePlayer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Player",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Player", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Player");
+        }
+    }
+}
