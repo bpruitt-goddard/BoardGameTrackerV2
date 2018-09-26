9c4bd7109270bb59b010f30f02cfedfa66e84a2e
Brian Pruitt-Goddard <pruittgoddard.b@gmail.com>
2018-09-25 21:27:49 -0700

    Feat: Add Game model

---
+++ BoardGameTrackerV2/Models/Game.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace BoardGameTrackerV2.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public GameType Type { get; set; }
+        [DisplayName("BoardGameGeek Identifier")]
+        public int BggId { get; set; }
+
+        public enum GameType
+        {
+            Mixed,
+            Competitive,
+            Cooperative
+        }
+    }
+}
